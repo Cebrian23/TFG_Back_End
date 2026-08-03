693f8a37e7886a48f93818f883ae0a769a68a04c
Miguel <miguelangel5cebrian@gmail.com>
2026-08-03 12:41:36 +0200

    ft. Actualización de los atributos del tipo Profesor

--- types/Personas/Profesor.ts
+++ types/Personas/Profesor.ts
@@ -1,16 +1,16 @@
 import { OptionalId } from "npm:mongodb"
 
 export type ProfesorDB = OptionalId<{
-    nombre: string;
-    apellido_1: string;
-    apellido_2?: string;
-    DNI: string;
-    prefijo_movil?: string;
-    numero_movil?: string;
-    email: string;
-    password: string;
-    rol: "Profesor";
-    universidad: string;
+    nombre: string,
+    apellido_1: string,
+    apellido_2?: string,
+    DNI: string,
+    prefijo_movil?: string,
+    numero_movil?: string,
+    email: string,
+    password: string,
+    rol: "Profesor",
+    universidad: string,
 }>
 
 export type Profesor = {
